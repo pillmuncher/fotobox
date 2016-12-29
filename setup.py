d821bd5c56b40b881062af79e8300c9a5d81e350
Mick Krippendorf <pillmuncher@users.noreply.github.com>
2016-12-29 19:35:41 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="fotobox",
+    version="0.2a",
+    packages=find_packages(),
+    install_requires=[
+        'picamera>=1.12',
+        'Pillow>=3.4.2',
+        'pygame>=1.9.2b8',
+        'fakeRPiGPIO>=0.3a0',
+        'Rx>=1.5.3',
+    ]
+)
