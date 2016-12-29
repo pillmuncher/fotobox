56aa10e0f635cc2fc2c935d6fbeb7759c28045ed
Mick Krippendorf <pillmuncher@users.noreply.github.com>
2016-12-29 23:37:50 +0100

    anleitung geändert

--- setup.py
+++ setup.py
@@ -8,10 +8,9 @@ setup(
     version="0.2a0",
     packages=find_packages(),
     install_requires=[
-        'fakeRPiGPIO>=0.3a0',
+        'RPi.GPIO',
         'picamera>=1.12',
         'Pillow>=3.4.2',
-        'pygame>=1.9.2',
         'Rx>=1.5.7',
     ]
 )
