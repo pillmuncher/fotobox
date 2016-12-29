293ff8155791a2b1441a472b95160a3a4e8148d4
Mick Krippendorf <pillmuncher@users.noreply.github.com>
2016-12-29 23:49:50 +0100

    __main__ gebaut

--- fotobox/__init__.py
+++ fotobox/__init__.py
@@ -249,18 +249,3 @@ def run(conf):
                         conf.exit_code.get()
                     finally:
                         lightshow(1, conf)
-
-
-if __name__ == '__main__':
-    import sys
-    import argparse
-    from .config import config
-    parser = argparse.ArgumentParser(description='FotoBox Programm.')
-    parser.add_argument(
-        'filename',
-        default='fotobox.json',
-        metavar='c',
-        type=str,
-        help='path to config file'
-    )
-    thread_thru(parser.parse_args().filename, config, run, sys.exit)
